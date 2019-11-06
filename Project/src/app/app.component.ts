542c141b60d4556f9327da2d68cf54b202ce9a38
Pavel <pahan.rolich@mail.ru>
2019-11-06 16:57:04 +0300

    project beta

--- Project/src/app/app.component.ts
+++ Project/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FactService } from './service/fact.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Project';
+  constructor(private http: HttpClient, private factService: FactService) {}
 }
--- Project/src/app/app.module.ts
+++ Project/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { FactScrollerComponent } from './fact-scroller/fact-scroller.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FactService } from './service/fact.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FactScrollerComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ScrollingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [FactService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
